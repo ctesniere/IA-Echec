83e5d89313dfeccfc005bcd7f4d1a52eee793608
philippeyuen <philippeyuen@gmail.com>
2013-07-02 23:01:53 +0200

    Created user interface with drag & drop capabilities

--- Client Java/src/main/java/fr/esgi/App.java
+++ Client Java/src/main/java/fr/esgi/App.java
@@ -1,15 +1,24 @@
 package fr.esgi;
 
+import javax.swing.JFrame;
+
+import fr.esgi.iuwindow.MainGraphique;
+
 /**
  * App
  * 
  * @author Cédric TESNIERE
  * @since 5 janv. 2013
  */
-public class App 
+public class App extends JFrame
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	JFrame frame = new MainGraphique();
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
     }
 }
--- Client Java/src/main/java/fr/esgi/iuwindow/MainGraphique.java
+++ Client Java/src/main/java/fr/esgi/iuwindow/MainGraphique.java
@@ -1,6 +1,22 @@
 package fr.esgi.iuwindow;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 /**
  * MainGraphique
@@ -8,17 +24,103 @@ import javax.swing.JFrame;
  * @author Cédric TESNIERE
  * @since 5 janv. 2013
  */
-public class MainGraphique {
+public class MainGraphique extends JFrame implements MouseListener, MouseMotionListener {
 
-	public static void main(String[] args) {
-		try {
-			JFrame fenetre = new JFrame("ChessBoard");
-			fenetre.setSize(400, 400);
-			fenetre.setVisible(true);
+	JLayeredPane layeredPane;
+	JPanel chessBoard;
+	JLabel chessPiece;
+	int xAdjustment;
+	int yAdjustment;
+	
+	public MainGraphique() {
+		Dimension boardSize = new Dimension(400, 400);
+		layeredPane = new JLayeredPane();
+		getContentPane().add(layeredPane);
+		layeredPane.setPreferredSize(boardSize);
+		layeredPane.addMouseListener(this);
+		layeredPane.addMouseMotionListener(this);
 
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
+		
+		chessBoard = new JPanel();
+		layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+		chessBoard.setLayout(new GridLayout(8, 8));
+		chessBoard.setPreferredSize(boardSize);
+		chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
+		for (int i = 0; i < 64; i++) {
+			JPanel square = new JPanel(new BorderLayout());
+			chessBoard.add(square);
+			int row = (i / 8) % 2;
+			if (row == 0)
+				square.setBackground(i % 2 == 0 ? Color.white : Color.black);
+			else
+				square.setBackground(i % 2 == 0 ? Color.black : Color.white);
 		}
+		// Add a few pieces to the board
+		JLabel piece = new JLabel(new ImageIcon("C:\\Users\\EndL\\Desktop\\king.png"));
+		JPanel panel = (JPanel) chessBoard.getComponent(0);
+		panel.add(piece);
+		piece = new JLabel(new ImageIcon("/home/vinod/amarexamples/chess1.jpg"));
+		panel = (JPanel) chessBoard.getComponent(15);
+		panel.add(piece);
+		piece = new JLabel(new ImageIcon("C:\\Users\\EndL\\Desktop\\king.jpg"));
+		panel = (JPanel) chessBoard.getComponent(16);
+		panel.add(piece);
+		piece = new JLabel(new ImageIcon("/home/vinod/amarexamples/camel.jpg"));
+		panel = (JPanel) chessBoard.getComponent(20);
+		panel.add(piece);
+		
+		JButton startButton = new JButton("Start");
+		layeredPane.add(startButton, JLayeredPane.DEFAULT_LAYER);
+	}
+
+	public void mousePressed(MouseEvent e) {
+		chessPiece = null;
+		Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+		if (c instanceof JPanel)
+			return;
+		Point parentLocation = c.getParent().getLocation();
+		xAdjustment = parentLocation.x - e.getX();
+		yAdjustment = parentLocation.y - e.getY();
+		chessPiece = (JLabel) c;
+		chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+		chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+		layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+	}
+
+	// Move the chess piece around
+	public void mouseDragged(MouseEvent me) {
+		if (chessPiece == null)
+			return;
+		chessPiece
+				.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
+	}
+
+	// Drop the chess piece back onto the chess board
+	public void mouseReleased(MouseEvent e) {
+		if (chessPiece == null)
+			return;
+		chessPiece.setVisible(false);
+		Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+		if (c instanceof JLabel) {
+			Container parent = c.getParent();
+			parent.remove(0);
+			parent.add(chessPiece);
+		} else {
+			Container parent = (Container) c;
+			parent.add(chessPiece);
+		}
+		chessPiece.setVisible(true);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	public void mouseMoved(MouseEvent e) {
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
 	}
 }
