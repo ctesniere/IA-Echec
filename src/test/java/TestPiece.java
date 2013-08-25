fe95263f89110086e2ed9654515ecb4ce805019f
ctesniere <tesniere.cedric@gmail.com>
2013-08-25 12:30:00 +0200

    mai des tests pour les pièces + Changement de méthode pour récupérer la valeur des noir ou des blanc (utilisé Algorithm.isBlack() et Algorithm.isWhite())

--- src/test/java/TestPiece.java
+++ src/test/java/TestPiece.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
+import fr.esgi.ia.Algorithm;
 import fr.esgi.ia.Chessboard;
 import fr.esgi.ia.Helper;
 import fr.esgi.ia.IA;
@@ -11,9 +12,9 @@ import fr.esgi.ia.Move;
 import fr.esgi.ia.Piece;
 
 /**
- * Permet de faire les tests sur les pieces de l'échiquier
+ * Permet de faire les tests sur les pieces de l'√©chiquier
  * 
- * @author Cédric TESNIERE
+ * @author C√©dric TESNIERE
  * @since 1
  */
 public class TestPiece {
@@ -24,22 +25,24 @@ public class TestPiece {
 	public void initChessboard() {
 		IA ia = new IA();
 
-		chessboard.insertPieceChessboard(ia.isBlack(), "d1", "e1", "c1:f1", "b1:g1", "a1:h1",
+		chessboard.insertPieceChessboard(Algorithm.isBlack(), "d1", "e1", "c1:f1", "b1:g1", "a1:h1",
 				"a2:b2:c2:d2:e2:f2:g2:h2");
-		chessboard.insertPieceChessboard(ia.isWhite(), "d8", "e8", "c8:f8", "b8:g8", "a8:h8",
+		chessboard.insertPieceChessboard(Algorithm.isWhite(), "d8", "e8", "c8:f8", "b8:g8", "a8:h8",
 				"a7:b7:c7:d7:e7:f7:g7:h7");
 	}
 
 	@Test
 	public void testKing() {
 		String location = "d1";
-		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
-
-		// Vérifie si la méthode trouve des positions
+		int x = Helper.getXfromString(location);
+		int y = Helper.getYfromString(location);
+		Piece piece = chessboard.getPiece(x, y);
+		
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 0);
 	}
 
@@ -48,11 +51,11 @@ public class TestPiece {
 		String location = "e1";
 		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
 
-		// Vérifie si la méthode trouve des positions
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 0);
 	}
 
@@ -61,11 +64,11 @@ public class TestPiece {
 		String location = "c1";
 		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
 
-		// Vérifie si la méthode trouve des positions
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 0);
 	}
 
@@ -74,11 +77,11 @@ public class TestPiece {
 		String location = "b1";
 		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
 
-		// Vérifie si la méthode trouve des positions
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 2);
 	}
 
@@ -87,24 +90,24 @@ public class TestPiece {
 		String location = "a1";
 		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
 
-		// Vérifie si la méthode trouve des positions
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 0);
 	}
 
 	@Test
 	public void testPawn() {
-		String location = "b1";
+		String location = "a2";
 		Piece piece = chessboard.getPiece(Helper.getXfromString(location), Helper.getYfromString(location));
 
-		// Vérifie si la méthode trouve des positions
+		// V√©rifie si la m√©thode trouve des positions
 		ArrayList<Move> moves = piece.generateMovesForThisPiece(chessboard);
 		Assert.notNull(moves);
 
-		// Vérifie si le déplacement suivant est possible
+		// V√©rifie si le d√©placement suivant est possible
 		//Assert.assertEquals(moves.size(), 2);
 	}
 
