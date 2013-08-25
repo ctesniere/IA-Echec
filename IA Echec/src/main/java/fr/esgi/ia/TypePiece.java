179467cfd9b068a3c075a01f939cb0970e25f0b1
ctesniere <tesniere.cedric@gmail.com>
2013-08-25 17:59:33 +0200

    chessboardValue(), Simplification de la méthode

---
+++ IA Echec/src/main/java/fr/esgi/ia/TypePiece.java
@@ -0,0 +1,34 @@
+package fr.esgi.ia;
+
+public enum TypePiece {
+
+	/**
+	 * @see Reine
+	 */
+	QUEEN,
+	
+	/**
+	 * @see Roi
+	 */
+    KING,
+    
+    /**
+     * @see Fou
+     */
+    BISHOP,
+    
+    /**
+     * @see Chevalier
+     */
+    KNIGHT,
+    
+    /**
+     * @see Tour
+     */
+    ROOK,
+
+    /**
+     * @see Pion
+     */
+    PAWN;
+}
