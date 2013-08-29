4e978b707755ca4549f31691fea1ed24d1439548
ctesniere <tesniere.cedric@gmail.com>
2013-08-29 14:15:05 +0200

    Ajout d'une nouvelle méthode afin de démarrer une partie avec les positions de départ par défaut

--- IA Echec/src/main/java/fr/esgi/controller/JSONController.java
+++ IA Echec/src/main/java/fr/esgi/controller/JSONController.java
@@ -14,8 +14,7 @@ import fr.esgi.ia.IA;
 @RequestMapping("/alphabeta")
 public class JSONController {
 
-	@RequestMapping(value = "/black/{bKing}/{bQueen}/{bCrazy}/{bKnight}/{bTower}/{bPawn}/white/{wKing}/{wQueen}/{wCrazy}/{wKnight}/{wTower}/{wPawn}",
-			method = RequestMethod.GET)
+	@RequestMapping(value = "/black/{bKing}/{bQueen}/{bCrazy}/{bKnight}/{bTower}/{bPawn}/white/{wKing}/{wQueen}/{wCrazy}/{wKnight}/{wTower}/{wPawn}", method = RequestMethod.GET)
 	@ResponseBody
 	public String getShopInJSON(@PathVariable String bKing, @PathVariable String bQueen,
 			@PathVariable String bCrazy, @PathVariable String bKnight, @PathVariable String bTower,
@@ -24,13 +23,15 @@ public class JSONController {
 			@PathVariable String wPawn) {
 
 		IA ia = new IA();
-		
+
 		Chessboard chessboard = new Chessboard();
-		chessboard.insertPieceChessboard(Algorithm.isBlack(), bKing, bQueen, bCrazy, bKnight, bTower, bPawn);
-		chessboard.insertPieceChessboard(Algorithm.isWhite(), wKing, wQueen, wCrazy, wKnight, wTower, wPawn);
-		
+		chessboard.insertPieceChessboard(Algorithm.isBlack(), bKing, bQueen, bCrazy, bKnight,
+				bTower, bPawn);
+		chessboard.insertPieceChessboard(Algorithm.isWhite(), wKing, wQueen, wCrazy, wKnight,
+				wTower, wPawn);
+
 		// Retourne le meilleur coup
-		return ia.play(ia.isMyColor(), 3, chessboard);
+		return ia.play(Algorithm.isWhite(), 3, chessboard);
 
 		// try {
 		// ObjectMapper mapper = new ObjectMapper();
@@ -42,5 +43,21 @@ public class JSONController {
 		// } catch (IOException e) {
 		// return e.getMessage();
 		// }
+	}
+
+	@RequestMapping(value = "/init", method = RequestMethod.GET)
+	@ResponseBody
+	public String getShopInJSON() {
+
+		IA ia = new IA();
+
+		Chessboard chessboard = new Chessboard();
+		chessboard.insertPieceChessboard(Algorithm.isBlack(), "d1", "e1", "c1:f1", "b1:g1",
+				"a1:h1", "a2:b2:c2:d2:e2:f2:g2:h2");
+		chessboard.insertPieceChessboard(Algorithm.isWhite(), "d8", "e8", "c8:f8", "b8:g8",
+				"a8:h8", "a7:b7:c7:d7:e7:f7:g7:h7");
+
+		// Retourne le meilleur coup
+		return ia.play(Algorithm.isWhite(), 3, chessboard);
 	}
 }
