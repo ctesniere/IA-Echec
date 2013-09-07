4dea66cf9cb5d0e3facb5cf8bec1ba930b524e0e
ctesniere <tesniere.cedric@gmail.com>
2013-09-07 19:05:43 +0200

    Affichage du JSON en sortie + Format

--- src/test/java/TestIAController.java
+++ src/test/java/TestIAController.java
@@ -1,10 +1,8 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import fr.esgi.ia.Algorithm;
 import fr.esgi.ia.Chessboard;
 import fr.esgi.ia.IA;
-import fr.esgi.ia.Piece;
 
 public class TestIAController {
 
@@ -15,7 +13,7 @@ public class TestIAController {
 		chessboard.insertPieceChessboard(Algorithm.isBlack(), "d8", "e8", "c8:f8", "b8:g8",
 				"a8:h8", "a7:b7:c5:d7:e7:f7:g7:h7");
 		chessboard.insertPieceChessboard(Algorithm.isWhite(), "d1", "e1", "c1:f1", "b1:g1",
-				"a1:h1", "a2:b4:c2:d2:e2:f2:g2:h4");
+				"a1:h1", "a2:b4:c2:d2:e2:f2:g2:h2");
 
 		IA ia = new IA();
 		System.out.println(ia.play(Algorithm.isWhite(), 2, chessboard));
