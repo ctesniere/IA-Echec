8acd4b7e26dee6fde649cf87da7a6d2d31ba180c
ctesniere <tesniere.cedric@gmail.com>
2013-09-07 12:55:50 +0200

    Changement des valeurs du test

--- src/test/java/TestIAController.java
+++ src/test/java/TestIAController.java
@@ -14,7 +14,7 @@ public class TestIAController {
 		Chessboard chessboard = new Chessboard();
 		chessboard.insertPieceChessboard(Algorithm.isBlack(), "d8", "e8", "c8:f8", "b8:g8",
 				"a8:h8", "a7:b7:c5:d7:e7:f7:g7:h7");
-		chessboard.insertPieceChessboard(Algorithm.isWhite(), "d4", "e1", "c1:f1", "c3:g1",
+		chessboard.insertPieceChessboard(Algorithm.isWhite(), "d1", "e1", "c1:f1", "b1:g1",
 				"a1:h1", "a2:b4:c2:d2:e2:f2:g2:h4");
 
 		IA ia = new IA();
